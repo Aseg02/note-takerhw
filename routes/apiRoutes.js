d592508008a25202f04d76f7ef3616a891b8fa33
Ana Segovia <anasegovia505@gmail.com>
2023-09-14 12:18:15 -0500

    commits

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,16 +4,13 @@ import { readFileSync, writeFileSync } from 'fs';
 import uniq from 'uniq';
 
 export default (app) => {
-
   app.get('/api/notes', (_req, res) => {
     res.sendFile(join(__dirname, '../db/db.json'));
   });
- 
   app.post('/api/notes', (req, res) => {
     let db = readFileSync('db/db.json');
     db = JSON.parse(db);
     res.json(db);
-
     let userNote = {
       title: req.body.title,
       text: req.body.text,
